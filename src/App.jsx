e8888f5c289186c4f68630f6c14d97fe9ae82172
Alaguraja0906 <alaguraja0906@gmail.com>
2023-10-02 15:09:46 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn.jsx';
+import SignUP from './pages/SignUp.jsx';
+import Profile from './pages/Profile.jsx';
+
 
 export default function App() {
-  return (
+  return <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/Sign-in" element={<SignIn/>}/>
+    <Route path="/Sign-up" element={<SignUP/>}/>
+    <Route path="/About" element={<About/> }/>
+    <Route path="/profile" element={<Profile/>}/>
+   </Routes>
+   
+  </BrowserRouter>;
      
-     <div>
-      <h1 className='text-green-500'>App</h1>
+     
+     
+  
 
-      <p className='text-red-300,font-roman'>This is my first full stack irrigation project</p>
-     </div>
-  )
+   
 }
