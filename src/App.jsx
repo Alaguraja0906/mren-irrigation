f5434b8f19cc5ad1285b6daf16ee04f906e18391
Alaguraja0906 <alaguraja0906@gmail.com>
2023-10-02 12:26:24 +0530

    install react js and tailwindcss and create first template

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+     
+     <div>
+      <h1 className='text-green-500'>App</h1>
+
+      <p className='text-red-300,font-roman'>This is my first full stack irrigation project</p>
+     </div>
+  )
+}
